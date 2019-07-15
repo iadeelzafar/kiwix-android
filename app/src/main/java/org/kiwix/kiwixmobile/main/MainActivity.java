863c8b9b3ce0ef9d68f97627451181623d9bd12b
Adeel Zafar <adeelzafar619@gmail.com>
2019-07-15 22:09:04 +0500

    Sample code to get details of html page from ZIM File

--- app/src/main/java/org/kiwix/kiwixmobile/main/MainActivity.java
+++ app/src/main/java/org/kiwix/kiwixmobile/main/MainActivity.java
@@ -24,12 +24,14 @@ import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.database.Cursor;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.media.AudioManager;
@@ -39,6 +41,8 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Environment;
 import android.os.Handler;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
@@ -100,7 +104,6 @@ import org.kiwix.kiwixmobile.base.BaseActivity;
 import org.kiwix.kiwixmobile.bookmark.BookmarkItem;
 import org.kiwix.kiwixmobile.bookmark.BookmarksActivity;
 import org.kiwix.kiwixmobile.data.ZimContentProvider;
-import org.kiwix.kiwixmobile.data.local.entity.Bookmark;
 import org.kiwix.kiwixmobile.help.HelpActivity;
 import org.kiwix.kiwixmobile.history.HistoryActivity;
 import org.kiwix.kiwixmobile.history.HistoryListItem;
@@ -922,6 +925,29 @@ public class MainActivity extends BaseActivity implements WebViewCallback,
         Intent intentSupportKiwix = new Intent(Intent.ACTION_VIEW, uriSupportKiwix);
         intentSupportKiwix.putExtra(EXTRA_EXTERNAL_LINK, true);
         openExternalUrl(intentSupportKiwix);
+        break;
+
+      case R.id.menu_start_server:
+        String pagePath,randomArticlePath,mainPagePath;
+        pagePath = ZimContentProvider.getPageUrlFromTitle(ZimContentProvider.getZimFileTitle());
+        randomArticlePath = ZimContentProvider.getRandomArticleUrl();
+        mainPagePath = ZimContentProvider.getMainPage();
+        Log.v("DANG","file Title : "+ZimContentProvider.getZimFileTitle());
+        Log.v("DANG","page Path :"+pagePath);
+        Log.v("DANG","CONTENT URI : "+ZimContentProvider.CONTENT_URI);
+        Log.v("DANG","RANDOM ARTICLE : "+randomArticlePath);
+        Log.v("DANG","Main Page : "+mainPagePath);
+        Log.v("DANG","ZIM FILE PATH: "+ZimContentProvider.getZimFile());
+        Uri uri = Uri.parse(ZimContentProvider.CONTENT_URI + pagePath);
+        Log.v("DANG","DEBUG 1");
+        File file = new File(uri.getPath());
+        Log.v("DANG","DEBUG 2");
+
+        //Crashes here.
+        String selectedFilePath = getPath(this, uri);
+        Log.v("DANG","URI : "+uri.toString());
+        Log.v("DANG","uri.getPath(): "+uri.getPath());
+        Log.v("DANG","selectedFilePath: "+selectedFilePath);
 
       default:
         break;
@@ -929,6 +955,131 @@ public class MainActivity extends BaseActivity implements WebViewCallback,
 
     return super.onOptionsItemSelected(item);
   }
+
+  public static boolean isExternalStorageDocument(Uri uri) {
+    return "com.android.externalstorage.documents".equals(uri
+        .getAuthority());
+  }
+
+  public static String getPath(final Context context, final Uri uri) {
+
+    // check here to KITKAT or new version
+    final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+    // DocumentProvider
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && DocumentsContract.isDocumentUri(
+        context, uri)) {
+
+      // ExternalStorageProvider
+      if (isExternalStorageDocument(uri)) {
+        final String docId = DocumentsContract.getDocumentId(uri);
+        final String[] split = docId.split(":");
+        final String type = split[0];
+
+        if ("primary".equalsIgnoreCase(type)) {
+          return Environment.getExternalStorageDirectory() + "/"
+              + split[1];
+        }
+      }
+      // DownloadsProvider
+      else if (isDownloadsDocument(uri)) {
+
+        final String id = DocumentsContract.getDocumentId(uri);
+        final Uri contentUri = ContentUris.withAppendedId(
+            Uri.parse("content://downloads/public_downloads"),
+            Long.valueOf(id));
+
+        return getDataColumn(context, contentUri, null, null);
+      }
+
+
+      // MediaProvider
+      else if (isMediaDocument(uri)) {
+        final String docId = DocumentsContract.getDocumentId(uri);
+        final String[] split = docId.split(":");
+        final String type = split[0];
+
+        Uri contentUri = null;
+        if ("image".equals(type)) {
+          contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        } else if ("video".equals(type)) {
+          contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+        } else if ("audio".equals(type)) {
+          contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        }
+
+        final String selection = "_id=?";
+        final String[] selectionArgs = new String[] { split[1] };
+
+        return getDataColumn(context, contentUri, selection,
+            selectionArgs);
+      }
+    }
+    // MediaStore (and general)
+    else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+      // Return the remote address
+      if (isGooglePhotosUri(uri)) {
+        return uri.getLastPathSegment();
+      }
+
+      return getDataColumn(context, uri, null, null);
+    }
+    // File
+    else if ("file".equalsIgnoreCase(uri.getScheme())) {
+      return uri.getPath();
+    }
+
+    return null;
+  }
+
+  public static boolean isDownloadsDocument(Uri uri) {
+    return "com.android.providers.downloads.documents".equals(uri
+        .getAuthority());
+  }
+
+  /**
+   * @param uri The Uri to check.
+   * @return Whether the Uri authority is MediaProvider.
+   */
+  public static boolean isMediaDocument(Uri uri) {
+    return "com.android.providers.media.documents".equals(uri
+        .getAuthority());
+  }
+
+  public static String getDataColumn(Context context, Uri uri,
+      String selection, String[] selectionArgs) {
+
+    Cursor cursor = null;
+    final String column = "_data";
+    final String[] projection = { column };
+
+    try {
+      cursor = context.getContentResolver().query(uri, projection,
+          selection, selectionArgs, null);
+      if (cursor != null && cursor.moveToFirst()) {
+        final int index = cursor.getColumnIndexOrThrow(column);
+        return cursor.getString(index);
+      }
+    } finally {
+      if (cursor != null) {
+        cursor.close();
+      }
+    }
+    return null;
+  }
+
+  /**
+
+  /**
+   * @param uri The Uri to check.
+   * @return Whether the Uri authority is Google Photos.
+   */
+  public static boolean isGooglePhotosUri(Uri uri) {
+    return "com.google.android.apps.photos.content".equals(uri
+        .getAuthority());
+  }
+
 
   /** Dialog to take user confirmation before deleting all notes */
   private void showClearAllNotesDialog() {
@@ -1716,6 +1867,7 @@ public class MainActivity extends BaseActivity implements WebViewCallback,
       menu.findItem(R.id.menu_random_article).setVisible(false);
       menu.findItem(R.id.menu_searchintext).setVisible(false);
       menu.findItem(R.id.menu_read_aloud).setVisible(false);
+      menu.findItem(R.id.menu_start_server).setVisible(false);
     } else {
       menu.findItem(R.id.menu_search).setVisible(true);
       menu.findItem(R.id.menu_fullscreen).setVisible(true);
@@ -1730,6 +1882,7 @@ public class MainActivity extends BaseActivity implements WebViewCallback,
         menu.findItem(R.id.menu_home).setVisible(true);
         menu.findItem(R.id.menu_random_article).setVisible(true);
         menu.findItem(R.id.menu_searchintext).setVisible(true);
+        menu.findItem(R.id.menu_start_server).setVisible(true);
       }
     }
     return true;
